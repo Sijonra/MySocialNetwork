1c44004ee3c7758b14e9f0b7b5f15214f448234f
Ilya <sijonra@gmail.com>
2022-06-08 19:12:00 +0300

    basic login form lay out

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,12 @@
+import style from './Login.module.scss'
+
 const Login = props =>{
     return(
-        <h1>Login Page</h1>
+        <section className={style.loginForm}>
+            <input type="text" placeholder='логин' className={style.login}/>
+            <input type="text" placeholder='пароль' className={style.password}/>
+            <input type="text" placeholder='ВОЙТИ' className={style.button}/>
+        </section>
     )
 }
 
